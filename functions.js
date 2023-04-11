5e4cf0a0460ffdde165c3b6322da09b2ff780113
kskannandotwin <kskannan.win@gmail.com>
2023-04-11 18:47:42 +0530

    11.04.2023
    223. creating functions has completed

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+// functions
+let getSimpleInterest = function (principle, rateOfInterest, noOfYears) {
+  let si = (principle * rateOfInterest * noOfYears) / 100;
+  return si;
+};
+
+let si1 = getSimpleInterest(1000, 6.7, 3);
+let si2 = getSimpleInterest(7000, 4.5, 10);
+console.log(si1);
+console.log(si2);
