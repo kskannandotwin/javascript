f8b971faddf02f40ef763480aa5c895d7b6c2300
kskannandotwin <kskannan.win@gmail.com>
2023-04-12 07:11:12 +0530

    12.04.2023
    225. function calls another function has completed

--- functions2.js
+++ functions2.js
@@ -1,7 +1,13 @@
 // functions with no arguments and return
 function showCity() {
-    console.log('New York');    
+  console.log("New York");
+  showCountry();
 }
+
+function showCountry() {
+  console.log("USA");
+}
+
 showCity();
 showCity();
 showCity();
